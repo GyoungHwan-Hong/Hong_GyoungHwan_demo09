5d1be6674351c36874b11310342990f03b8d5936
GyoungHwan-Hong <hongkh5218@gmail.com>
2021-11-02 11:28:22 -0700

    Add js.file.

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: ,
-  authDomain: ,
-  projectId: ,
-  storageBucket: ,
-  messagingSenderId: ,
-  appId: 
-};
+var  firebaseConfig = {
+  apiKey: "AIzaSyDN8vIl9mp5gk_73rwP2-6h8MqiMjq41eg",
+  authDomain: "demo08-comp1800-a95ea.firebaseapp.com",
+  projectId: "demo08-comp1800-a95ea",
+  storageBucket: "demo08-comp1800-a95ea.appspot.com",
+  messagingSenderId: "902331516254",
+  appId: "1:902331516254:web:33214adf6336c360b4fbcf"
+  };
   
 
 const app = firebase.initializeApp(firebaseConfig);
